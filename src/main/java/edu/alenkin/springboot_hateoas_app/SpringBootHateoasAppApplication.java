ea5f92bf6311f90525c24b262a1b34869b5a4b5e
AndrewAlyonkin <oxqq@ya.ru>
2021-07-12 13:11:59 +0800

    init

---
+++ src/main/java/edu/alenkin/springboot_hateoas_app/SpringBootHateoasAppApplication.java
@@ -0,0 +1,13 @@
+package edu.alenkin.springboot_hateoas_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootHateoasAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootHateoasAppApplication.class, args);
+    }
+
+}
